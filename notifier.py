d14f115a7ff8691550f4d86b458675fd5aff2407
bgheneti <bgheneti@mit.edu>
2014-09-12 23:24:32 -0400

    working  speech from google with british guy at end

--- notifier.py
+++ notifier.py
@@ -1,6 +1,9 @@
 import gmail
 import pyglet
 import time
+import urllib
+import urllib2
+
 ##import pyttsx
 myLists=['free-food@mit.edu','reuse@mit.edu']
 displayMail=[['a','b','c'] for i in range(5)]
@@ -8,8 +11,8 @@ displayMail=[['a','b','c'] for i in range(5)]
 '''
 __docformat__ = 'restructuredtext'
 window = pyglet.window.Window(fullscreen=True)
+music = pyglet.media.Player()
 pyglet.gl.glClearColor(1, 1, 1, 1)
-notification = pyglet.media.load('target_acquired1.wav', streaming=False)
 
 def get_mail(user,passwd,list):
     g = gmail.login(user, passwd)
@@ -18,7 +21,7 @@ def get_mail(user,passwd,list):
     i=0
     currentMail=[]
     for email in emails:
-        if(i==5):
+        if(i==3):
             break
         email.fetch()
         email.read()
@@ -64,19 +67,61 @@ def on_draw():
                           multiline=True,
                           anchor_x='center', anchor_y='top').draw()
 
+def voicePlayer(newMail):
+    queue=[]
+    checker=[]
+    global music
+    for mail in newMail:
+        checker.append(mail[0])
+    while(len(checker)>0):
+        print "checker length", len(checker)
+        packets=[]
+        text=checker[0]
+        if len(text)>40:
+            i=20
+            while(text[i]!=' 'and i<len(text)-2):
+                i+=1
+            if i==len(text)-1:
+                print text
+                queue.append(urllib.quote_plus(text))
+                checker.pop(0)          
+            else:
+                print text[0:i]
+                queue.append(urllib.quote_plus(text[0:i]))
+                checker[0]=text[i+1:]         
+        else:
+            print text
+            queue.append(urllib.quote_plus(text))
+            checker.pop(0)
+    music = pyglet.media.Player()  
+    for text in queue:
+        print text
+        url = "https://translate.google.com/translate_tts?tl=en&q="+text
+        request = urllib2.Request(url)
+        request.add_header('User-agent', 'Mozilla/30.0') 
+        opener = urllib2.build_opener()
+        f = open("data.mp3", "wb")
+        f.write(opener.open(request).read())
+        f.close()
+        source = pyglet.media.load('data.mp3')
+        music.queue(source)
+    feeling=pyglet.media.load('feeling.wav')
+    music.queue(feeling)
+    music.play()
+    
 @window.event
 def notifier(dt,eLists=myLists,loud=True):
-    playMessage=False
-    global displayMail
-    for e in eLists:
+    if music.playing==False:
+        playMessage=False
         global displayMail
-        newMail=get_mail('user','pass', e)
-        if newMail!=[]:
-            playMessage=True
-        newMail.extend(displayMail)
-        displayMail=newMail[:5]
-    if playMessage:
-        notification.play()
+        for e in eLists:
+            newMail=get_mail('user','pass', e)
+            if newMail!=[]:
+                oldMail=displayMail
+                displayMail=[x for x in newMail]
+                displayMail.extend(oldMail)
+                displayMail=displayMail[:5]
+                voicePlayer(newMail)
 
 ##engine = pyttsx.init()
 ##engine.say("Cruft..... One. An old piece of computer equipment, possibly useless, that keeps hanging around. Two. An old alum. Reuse. This is where you find it.")
